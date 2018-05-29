a92936803520ddc31095a8afbf4afc0b9c30db7f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 20:39:59 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require 'pry'
 require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
-
-desc 'outputs hello to the terminal'
-task :hello do
-  puts "hello from Rake!"
-end
